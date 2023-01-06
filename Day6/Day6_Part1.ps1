efd0a3cb4af7fc356612b6405033c6a6b0fa7daf
yyyongdev <yyyongdev@gmail.com>
2023-01-06 15:17:28 +0900

    Day 6

---
+++ Day6/Day6_Part1.ps1
@@ -0,0 +1,36 @@
+﻿$line = Get-Content "$PSScriptRoot/Day6.txt"
+
+$bit = 1
+$bitTable = @{}
+foreach($i in 'a'..'z') {
+	$bitTable[$i] = $bit
+	$bit += $bit
+}
+
+$checkRange = 4
+
+$len = $line.Length
+$count = 0
+for ($i = $checkRange; $i -lt $len; $i++) {
+	$count = $i
+	$compare = 0
+	$isMarker = $true
+	for ($j = $i - $checkRange; $j -ne $i; $j++) {
+		$key = $line[$j]
+		$value = $bitTable[$key]
+		$check = $compare -band $value
+		if ($check -eq 0) {
+			$compare = $compare -bor $value
+		}
+		else {
+			$isMarker = $false
+			break;
+		}
+	}
+	if ($isMarker) {
+		break;
+	}
+
+}
+Write-Host "Result:" $count
+
