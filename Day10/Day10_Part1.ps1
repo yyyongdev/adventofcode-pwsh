f48576099231b5e9bc6edbdb077a0b0e41d8ee31
yongjun lee <yyyongdev@gmail.com>
2023-01-21 23:30:14 +0900

    Day9~11

---
+++ Day10/Day10_Part1.ps1
@@ -0,0 +1,28 @@
+$lines = Get-Content "$PSScriptRoot/Day10.txt"
+
+$value = 1
+$totalLoop = 0
+$result = 0
+
+$lines | ForEach-Object {
+	$command, $add = $_ -Split ' '
+
+	$loop = switch ($command) {
+		"noop" { 1 }
+		"addx" { 2 }
+	}
+
+	for ($i = 0; $i -lt $loop; $i++) {
+		$totalLoop++
+		if ($totalLoop -ge 20 -and $totalLoop -le 220) {
+			if (($totalLoop - 20) % 40 -eq 0) {
+				$result += ($totalLoop * $value)
+			}
+		}
+		if ($i -eq 1) {
+			$value += [int]$add
+		}
+	}
+}
+
+Write-Host "Result:" $result
